5792eab75d559752a1ba30861a83f5a968e21aad
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-06-03 20:23:13 -0700

    Add Book constructor and array to store books

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Odin Library");
+const myLibrary = Array();
+
+function Book(title, author, pageNum) {
+	if (!new.target) {
+    	throw Error("Use 'new' to declare new objects");
+    }
+    
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+}
+
+function addBookToLibrary(title, author, pageNum) {
+	myLibrary.push(new Book(title, author, pageNum));
+}
+
+addBookToLibrary("The Book", "Bob", 300);
+console.log(myLibrary);
